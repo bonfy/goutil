16e6b4b2dce3617a1ae88400ef10aafde74b4e5c
bonfy <kai.chen@bonfy.local>
2017-12-19 10:54:44 +0800

    update helper add Must func

--- helper.go
+++ helper.go
@@ -5,10 +5,18 @@ import (
 	"log"
 )
 
-// FailOnError panic msg when error occurs
+// FailOnError panic msg and err if error occurs
 func FailOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
 		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+// Must panic err if error occurs
+func Must(err error) {
+	if err != nil {
+		log.Fatalf("%s", err)
+		panic(err)
 	}
 }
